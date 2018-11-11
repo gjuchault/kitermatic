cc68d9e1dbb66103ca0fd8051c568b9862a3d2a1
Gabriel Juchault <gabriel.juchault@gmail.com>
2018-11-11 16:53:12 +0100

    fix(env): do not use .env

--- src/env.js
+++ src/env.js
@@ -1,12 +1,12 @@
-import path from 'path'
-import dotenv from 'dotenv'
-
-const result = dotenv.config({ path: path.resolve(__dirname, '..', '.env') })
-
 export const number = (value, defaultValue = 0) => {
   value = parseInt(value, 10)
 
   return Number.isFinite(value) ? value : defaultValue
 }
 
-export default result.parsed
+export default {
+  KTRM_UI_THEME_BG: process.env.KTRM_UI_THEME_BG || 'cyan',
+  KTRM_UI_THEME_FG: process.env.KTRM_UI_THEME_FG || 'black',
+  KTRM_REFRESH_INTERVAL: number(process.env.KTRM_REFRESH_INTERVAL, 3000),
+  KTRM_LOGS_TAIL: number(process.env.KTRM_LOGS_TAIL, 100)
+}
